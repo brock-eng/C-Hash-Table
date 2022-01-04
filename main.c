595a02e604c719cd1c7954871a40fd87444761d7
Zach Brock <zbrock.eng@gmail.com>
2022-01-04 15:13:09 -0600

    initial commit

--- main.c
+++ main.c
@@ -1,1 +1,20 @@
 
+#define SIZE 50000
+
+typedef struct Ht_item Ht_item;
+
+// Defined Hash Table
+struct Ht_item
+{
+   char* key;
+   char* value;
+};
+
+// Hashing function
+unsigned long HashFunction(char* string)
+{
+   unsigned long hashCounter = 0;
+   for (int i; string[i]; i++)
+      hashCounter += string[i];
+   return hashCounter % SIZE;
+}
